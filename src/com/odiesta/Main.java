3a9465fb3d601396cbc57db4c7f5f0cbf69e73af
Odiesta Shandikarona <40514327+Odiesta@users.noreply.github.com>
2020-10-31 11:41:32 +0800

    Update Main.java

--- src/com/odiesta/Main.java
+++ src/com/odiesta/Main.java
@@ -10,34 +10,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        VBox root = new VBox();
-//
-//        Path path = new Path();
-//
-//        MoveTo moveTo = new MoveTo();
-//        moveTo.setX(50.0);
-//        moveTo.setY(50.0);
-//
-//        CubicCurveTo cubicCurveTo = new CubicCurveTo();
-//        cubicCurveTo.setControlX1(50.0);
-//        cubicCurveTo.setControlX2(50.0);
-//        cubicCurveTo.setControlY1(50.0);
-//        cubicCurveTo.setControlY2(50.0);
-//        cubicCurveTo.setX(150.0);
-//        cubicCurveTo.setY(150.0);
-//
-//        ClosePath closePath = new ClosePath();
-//
-//        path.getElements().addAll(moveTo, cubicCurveTo, closePath);
-//
-//        root.getChildren().add(path);
-//
-//        Scene scene = new Scene(root, 500, 400);
-//        primaryStage.setTitle("Path test");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-
-
         Parent root = FXMLLoader.load(getClass().getResource("pathElement.fxml"));
         primaryStage.setTitle("Path Element Creator");
         primaryStage.setScene(new Scene(root, 700, 575));
